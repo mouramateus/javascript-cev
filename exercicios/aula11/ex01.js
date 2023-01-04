2ffa12ed8e6c004cbd44fff2ea10d56c5726c656
Mateus Moura <119609031+mouramateus@users.noreply.github.com>
2023-01-04 17:23:07 -0300

    novos exercícios

---
+++ exercicios/aula11/ex01.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro era ${vel}Km/h.`)
+if (vel > 60) {
+  console.log('Você foi multado por excesso de velocidade!')
+}
+console.log('Diriga sempre com segurança.')

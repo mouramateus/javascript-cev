31b531e044044afa9ee599dce1b16e1d42e8f290
Mateus Moura <119609031+mouramateus@users.noreply.github.com>
2023-01-06 13:19:49 -0300

    nnovos exercícios

---
+++ exercicios/aula12/ex01.js
@@ -0,0 +1,9 @@
+var idade = 68
+console.log('Sua idade é ' + idade)
+if (idade < 16) {
+  console.log('Não vota.')
+} else if (idade < 18 || idade > 67) {
+  console.log('Voto opcional.')
+} else {
+  console.log('Voto obrigatório.')
+}
